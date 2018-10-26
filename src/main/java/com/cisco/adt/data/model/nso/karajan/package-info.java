e40670e7a4b38a458664f29ab0e212a328e31ac1
sbraicu <sbraicu@cisco.com>
2018-10-26 09:43:11 +0200

    notification support

---
+++ src/main/java/com/cisco/adt/data/model/nso/karajan/package-info.java
@@ -0,0 +1,6 @@
+@javax.xml.bind.annotation.XmlSchema(namespace = "http://com/cisco/adt", xmlns = {
+		@XmlNs(prefix = "", namespaceURI = "http://com/cisco/adt") }, elementFormDefault = XmlNsForm.QUALIFIED)
+package com.cisco.adt.data.model.nso.karajan;
+
+import javax.xml.bind.annotation.XmlNs;
+import javax.xml.bind.annotation.XmlNsForm;
